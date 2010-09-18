d41615df95d6ddf2b0c9de2a70ba0ae98e5133b3
jammus <jammus@gmail.com>
2010-09-18 16:01:41 +0100

    Data is terminated by a new line

--- tests/lastfm-recenttracks-parser-test.js
+++ tests/lastfm-recenttracks-parser-test.js
@@ -11,24 +11,28 @@ ntest.describe("recentracks")
     assert.equal(null, this.parser.parse(''));
     })
 
+  ntest.it("won't parse without terminator", function() {
+    assert.equal(null, this.parser.parse('{\"recenttracks\":{\"track\":42}}'));
+    })
+
   ntest.it("returns object for value of recenttracks.track", function() {
-    assert.equal(42, this.parser.parse('{\"recenttracks\":{\"track\":42}}'));
+    assert.equal(42, this.parser.parse('{\"recenttracks\":{\"track\":42}}\n'));
    })
 
   ntest.it("returns null for unexpected input", function() {
-    assert.equal(null, this.parser.parse('{\"recentevents\":{\"event\":{}}}'));
+    assert.equal(null, this.parser.parse('{\"recentevents\":{\"event\":{}}}\n'));
   })
 
   ntest.it("returns most first track when array", function() {
-    assert.equal('first', this.parser.parse('{\"recenttracks\":{\"track\":[\"first\", \"second\"]}}'));
+    assert.equal('first', this.parser.parse('{\"recenttracks\":{\"track\":[\"first\", \"second\"]}}\n'));
   })
 
   ntest.it("returns null for non-json input", function() {
-    assert.equal(null, this.parser.parse('fi30i\ 32'));
+    assert.equal(null, this.parser.parse('fi30i\ 32\n'));
   })
 
   ntest.it("accepts input in chunks", function() {
     this.parser.parse('{\"recenttracks\":{\"trac');
-    var track = this.parser.parse('k\":42}}');
+    var track = this.parser.parse('k\":42}}\n');
     assert.equal(42, track);
   })
