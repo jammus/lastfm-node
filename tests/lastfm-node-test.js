bfa6810bfff398722b1b90786708b654d4f8479d
jammus <jammus@gmail.com>
2010-09-30 00:00:04 +0100

    Added ability to sign requests

--- tests/lastfm-node-test.js
+++ tests/lastfm-node-test.js
@@ -5,6 +5,7 @@ var FakeTracks = require('./TestData.js').FakeTracks;
 var assert = require('assert');
 var sys = require('sys');
 var ntest = require('ntest');
+var crypto = require('crypto');
 
 ntest.describe("default LastFmNode instance")
   ntest.before(function() { this.lastfm = new LastFmNode(); })
@@ -26,6 +27,30 @@ ntest.describe("default LastFmNode instance")
     this.lastfm.params = { foo : "bar", baz : "bash" };
     additionalParams = { flip : "flop" };
     assert.equal("/2.0?foo=bar&baz=bash&flip=flop", this.lastfm.requestUrl(additionalParams));
+  });
+
+  ntest.it("additional params leaves original untouched", function() {
+   this.lastfm.params = { foo: "bar" };
+   var url = this.lastfm.requestUrl({ foo: "baz" });
+   assert.equal("bar", this.lastfm.params.foo);         
+  });
+
+  ntest.it("signed requests include hash of params plus secret", function() {
+    // see http://www.last.fm/api/webauth#6
+    var lastfm = new LastFmNode({ secret: 'secret' });
+    lastfm.params = { foo : "bar" };
+    var expectedHash =  crypto.createHash("md5").update("foobarsecret").digest("hex");
+    var url = lastfm.requestUrl(null, true);
+    assert.equal("/2.0?foo=bar&api_sig=" + expectedHash, url);
+  });
+
+  ntest.it("signed request hash order params alphabetically", function() {
+    // see http://www.last.fm/api/webauth#6
+    var lastfm = new LastFmNode({ secret: 'secret' });
+    lastfm.params = { foo : "bar", baz: "bash" };
+    var url = lastfm.requestUrl({ flip : "flop"}, true);
+    var expectedHash = crypto.createHash("md5").update("bazbashflipflopfoobarsecret").digest("hex");
+    assert.equal("/2.0?foo=bar&baz=bash&flip=flop&api_sig=" + expectedHash, url);
   });
 
 ntest.describe("LastFmNode instance")
