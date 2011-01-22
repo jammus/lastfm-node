cb69c58ac94ae714fb3652346bab10fbde30f7b5
jammus <jammus@gmail.com>
2011-01-22 23:32:55 +0000

    Tidy tests (removing thises)

--- tests/lastfm-info-test.js
+++ tests/lastfm-info-test.js
@@ -3,146 +3,147 @@ var LastFmInfo = require("lastfm/lastfm-info");
 var LastFmRequest = require("lastfm/lastfm-request");
 var fakes = require("./fakes");
 
+(function() {
 describe("a new info instance")
+  var lastfm, gently;
   before(function() {
-    this.lastfm = new LastFmNode();
-    this.gently = new Gently();
+    lastfm = new LastFmNode();
+    gently = new Gently();
   });
 
   it("accepts listeners in options", function() {
     var handlers = { error: function() {}, success: function() {} };
-    this.gently.expect(handlers, "error");
-    this.gently.expect(handlers, "success");
-    var info = new LastFmInfo(this.lastfm, "", handlers);
+    gently.expect(handlers, "error");
+    gently.expect(handlers, "success");
+    var info = new LastFmInfo(lastfm, "", handlers);
     info.emit("success");
   });
   
   it("emits error if type not specified", function() {
     var handler = { error: function() {}};
-    this.gently.expect(handler, "error", function(error) {
+    gently.expect(handler, "error", function(error) {
       assert.equal("Item type not specified", error.message);
     });
-    var info = new LastFmInfo(this.lastfm, "", { error: handler.error });
+    var info = new LastFmInfo(lastfm, "", { error: handler.error });
   });
   
   it("allows requests for user info", function() {
-    this.gently.expect(this.lastfm, "read", function() {
+    gently.expect(lastfm, "read", function() {
       return new fakes.LastFmRequest();
     });
-    var info = new LastFmInfo(this.lastfm, "user");
+    var info = new LastFmInfo(lastfm, "user");
   });
 
   it("allows requests for track info", function() {
-    this.gently.expect(this.lastfm, "read", function() {
+    gently.expect(lastfm, "read", function() {
       return new fakes.LastFmRequest();
     });
-    var info = new LastFmInfo(this.lastfm, "track");
+    var info = new LastFmInfo(lastfm, "track");
   });
 
   it("allows all [itemtype].getinfo calls", function() {
-    this.gently.expect(this.lastfm, "read", function(params) {
+    gently.expect(lastfm, "read", function(params) {
       assert.equal("event.getinfo", params.method);
       return new fakes.LastFmRequest();
     });
-    new LastFmInfo(this.lastfm, "event");
+    new LastFmInfo(lastfm, "event");
   });
   
   it("calls unsigned methods", function() {
-    this.gently.expect(this.lastfm, "read", function(params, signed) {
+    gently.expect(lastfm, "read", function(params, signed) {
       assert.equal(false, signed);
       return new fakes.LastFmRequest();
     });
-    var info = new LastFmInfo(this.lastfm, "user");
+    var info = new LastFmInfo(lastfm, "user");
   });
 
   it("passes through parameters", function() {
-    this.gently.expect(this.lastfm, "read", function(params) {
+    gently.expect(lastfm, "read", function(params) {
       assert.equal("username", params.user);
       assert.equal("anything", params.arbitrary);
       return new fakes.LastFmRequest();
     });
-    new LastFmInfo(this.lastfm, "user", { user: "username", arbitrary: "anything" });
+    new LastFmInfo(lastfm, "user", { user: "username", arbitrary: "anything" });
   });
 
   it("doesnt pass through callback parameters", function() {
-    this.gently.expect(this.lastfm, "read", function(params) {
+    gently.expect(lastfm, "read", function(params) {
       assert.ok(!params.error);
       assert.ok(!params.success);
       return new fakes.LastFmRequest();
     });
-    new LastFmInfo(this.lastfm, "user", { error: function() {}, success: function() {} });
+    new LastFmInfo(lastfm, "user", { error: function() {}, success: function() {} });
   });
+})();
 
+(function() {
 describe("when receiving data")
+  var gently, lastfm, request;
   before(function() {
-    this.gently = new Gently();
-    this.lastfm = new LastFmNode();
-    this.request = new fakes.LastFmRequest();
+    gently = new Gently();
+    lastfm = new LastFmNode();
+    request = new fakes.LastFmRequest();
   });
 
   it("emits error if response contains error", function() {
-    var that = this;
-    this.gently.expect(this.lastfm, "read", function(params, signed) {
-      return that.request;
+    gently.expect(lastfm, "read", function(params, signed) {
+      return request;
     });
-    new LastFmInfo(this.lastfm, "track", {
-      error: this.gently.expect(function errorHandler(error) {
+    new LastFmInfo(lastfm, "track", {
+      error: gently.expect(function errorHandler(error) {
         assert.equal("You must supply either a track & artist name or a track mbid.", error.message);
       })
     });
-    this.request.emit("success", FakeData.NotEnoughTrackInfo);
+    request.emit("success", FakeData.NotEnoughTrackInfo);
   });
 
   it("emits error when receiving unexpected data", function() {
-    var that = this;
-    this.gently.expect(this.lastfm, "read", function(params, signed) {
-      return that.request;
+    gently.expect(lastfm, "read", function(params, signed) {
+      return request;
     });
-    new LastFmInfo(this.lastfm, "track", {
-      error: this.gently.expect(function errorHandler(error) {
+    new LastFmInfo(lastfm, "track", {
+      error: gently.expect(function errorHandler(error) {
         assert.equal("Unexpected error", error.message);
       })
     });
-    this.request.emit("success", FakeData.SuccessfulAuthorisation);
+    request.emit("success", FakeData.SuccessfulAuthorisation);
   });
 
   it("emits error if receiving junk", function() {
-      var that = this;
-      this.gently.expect(this.lastfm, "read", function(params, signed) {
-        return that.request;
+      gently.expect(lastfm, "read", function(params, signed) {
+        return request;
       });
-      new LastFmInfo(this.lastfm, "track", {
-        error: this.gently.expect(function errorHandler(error) {
+      new LastFmInfo(lastfm, "track", {
+        error: gently.expect(function errorHandler(error) {
           assert.ok(error.message.indexOf(FakeData.Garbage) > -1);
           assert.ok("Syntax error");
         })
       });
-      this.request.emit("success", FakeData.Garbage);
+      request.emit("success", FakeData.Garbage);
   });
 
   it("emits success with received data when matches expected type", function() {
-    var that = this;
-    this.gently.expect(this.lastfm, "read", function(params, signed) {
-        return that.request;
+    gently.expect(lastfm, "read", function(params, signed) {
+        return request;
     });
-    new LastFmInfo(this.lastfm, "track", {
-      success: this.gently.expect(function success(track) {
+    new LastFmInfo(lastfm, "track", {
+      success: gently.expect(function success(track) {
         assert.equal("Run To Your Grave", track.name);
         assert.equal("232000", track.duration);
       })
     });
-    this.request.emit("success", FakeData.RunToYourGraveTrackInfo);
+    request.emit("success", FakeData.RunToYourGraveTrackInfo);
   });
 
   it("bubbles up errors", function() {
-    var that = this;
-    this.gently.expect(this.lastfm, "read", function(params, signed) {
-        return that.request;
+    gently.expect(lastfm, "read", function(params, signed) {
+        return request;
     });
-    var info = new LastFmInfo(this.lastfm, "track");
-    this.gently.expect(info, "emit", function(event, error) {
+    var info = new LastFmInfo(lastfm, "track");
+    gently.expect(info, "emit", function(event, error) {
       assert.equal("error", event);
       assert.equal("Bubbled error", error.message);
     });
-    that.request.emit("error", new Error("Bubbled error"));
+    request.emit("error", new Error("Bubbled error"));
   });
+})();
