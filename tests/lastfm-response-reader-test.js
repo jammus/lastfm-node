682c334f77b76873dfcc112e0833dffb0c17cdae
jammus <jammus@gmail.com>
2011-01-22 12:29:07 +0000

    Remove ugly thises

--- tests/lastfm-response-reader-test.js
+++ tests/lastfm-response-reader-test.js
@@ -1,40 +1,40 @@
-var assert = require('assert');
-var ntest = require('ntest');
-var FakeData = require('./TestData.js').FakeData;
+require("./common");
 var EventEmitter = require('events').EventEmitter;
 ResponseReader = require('../lib/lastfm-node/response-reader').ResponseReader;
 
-ntest.describe("receiver")
-  ntest.before(function() {
-    var mockRequest = new EventEmitter();
-    this.data = '';
-    var that = this;
-    this.reader = new ResponseReader(mockRequest, function(data) {
-      that.data = data;    
+(function() {
+describe("receiver")
+  var mockRequest, reader, mockResponse;
+  before(function() {
+    mockRequest = new EventEmitter();
+    data = '';
+    reader = new ResponseReader(mockRequest, function(receivedData) {
+      data = receivedData;    
     });
 
-    this.mockResponse = new EventEmitter();
-    mockRequest.emit("response", this.mockResponse);
+    mockResponse = new EventEmitter();
+    mockRequest.emit("response", mockResponse);
   });
 
-  ntest.it("fires callback on end", function() {
-    this.mockResponse.emit("data", FakeData.SingleRecentTrack);
-    this.mockResponse.emit("end");
-    assert.ok(this.data);
+  it("fires callback on end", function() {
+    mockResponse.emit("data", FakeData.SingleRecentTrack);
+    mockResponse.emit("end");
+    assert.ok(data);
   });
 
-  ntest.it("passes received data to callback", function() {
-    this.mockResponse.emit("data", FakeData.SingleRecentTrack);
-    this.mockResponse.emit("end");
-    assert.equal(FakeData.SingleRecentTrack, this.data);
+  it("passes received data to callback", function() {
+    mockResponse.emit("data", FakeData.SingleRecentTrack);
+    mockResponse.emit("end");
+    assert.equal(FakeData.SingleRecentTrack, data);
   });
 
-  ntest.it("accepts input in chunks", function() {
+  it("accepts input in chunks", function() {
     var first_chunk = FakeData.SingleRecentTrack.substr(0, 8);
     var second_chunk = FakeData.SingleRecentTrack.substr(8);
-    this.mockResponse.emit("data", first_chunk);
-    this.mockResponse.emit("data", second_chunk);
-    this.mockResponse.emit("end");
-    assert.ok(this.data);
-    assert.equal(FakeData.SingleRecentTrack, this.data);
+    mockResponse.emit("data", first_chunk);
+    mockResponse.emit("data", second_chunk);
+    mockResponse.emit("end");
+    assert.ok(data);
+    assert.equal(FakeData.SingleRecentTrack, data);
   });
+})();
